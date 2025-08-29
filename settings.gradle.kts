c5060ab144544faf3455ac6f4fd3dbc4b29d338b
suvenyra-afk <suvenyra@gmail.com>
2025-08-30 00:43:32 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,15 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
+        gradlePluginPortal()
     }
 }
-rootProject.name = "WordCounterApp"
+
+rootProject.name = "TavoProjektoPavadinimas"
 include(":app")
